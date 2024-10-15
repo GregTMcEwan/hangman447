9010ec159056128d5acd017c061b35e4245b853a
Gregor McEwan <gregortmcewan@gmail.com>
2024-10-15 13:40:42 +0100

    Implemented a function which takes in user inputs, validates them to ensure they are a single non numeric character and then returns it for further use.

---
+++ hangman/milestone_2.py
@@ -0,0 +1,17 @@
+import random
+
+word_list =["Apple", "Bannana", "Strawberry", "Pear", "Dragonfruit"]
+print(word_list)
+word = random.choice(word_list)
+print(word)
+
+def get_user_guess():
+    while True:
+        guess = input("Input your guess: ")
+        if len(guess) == 1 and guess.isdigit() == False:
+            print("Good guess!")
+            return guess
+        else:
+            print("Oops! That is not a valid input.")
+
+guess = get_user_guess()
